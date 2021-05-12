69472dbd3b11ca9a03b22ac1618b964113c5370f
Denis Evsyukov <denis@evsyukov.org>
2021-05-12 17:25:28 +0300

    [~] cmd parser

--- src/cmd.rs
+++ src/cmd.rs
@@ -1,69 +1,62 @@
-const HELP: &str = "\
-App
-
-USAGE:
-  app [OPTIONS] --number NUMBER [INPUT]
+const HELP: &str = concat!(
+    env!("CARGO_PKG_NAME"),
+    ": v.",
+    env!("CARGO_PKG_VERSION"),
+    "\n\n",
+    "USAGE:
+  app [OPTIONS] --file FILE --login LOGIN
 
 FLAGS:
   -h, --help            Prints help information
 
 OPTIONS:
-  --number NUMBER       Sets a number
+  --special STR         Whitelist of special characters, i.e: '_&#'
   --opt-number NUMBER   Sets an optional number
-  --width WIDTH         Sets width [default: 10]
-  --output PATH         Sets an output path
-
-ARGS:
-  <INPUT>
-";
+  --length 30           Length of the password, default=30
+  -c, --clip            Copy the generated password into the clipboard instead of displaying
+  --scramble-func md5   Hashing function to use for input data scrambling, default=md5.
+"
+);
 
 #[derive(Debug)]
 pub struct AppArgs {
-    number: u32,
-    opt_number: Option<u32>,
-    width: u32,
-    input: std::path::PathBuf,
-    output: Option<std::path::PathBuf>,
+    file: String,
+    login: String,
+    special: String,
+    length: u32,
+    clip: bool,
+    func: String,
 }
 
 pub fn parse_args() -> Result<AppArgs, pico_args::Error> {
     let mut pargs = pico_args::Arguments::from_env();
-
-    // Help has a higher priority and should be handled separately.
     if pargs.contains(["-h", "--help"]) {
         print!("{}", HELP);
         std::process::exit(0);
     }
 
-    let args = AppArgs {
-        // Parses a required value that implements `FromStr`.
-        // Returns an error if not present.
-        number: pargs.value_from_str("--number")?,
-        // Parses an optional value that implements `FromStr`.
-        opt_number: pargs.opt_value_from_str("--opt-number")?,
-        // Parses an optional value from `&str` using a specified function.
-        width: pargs
-            .opt_value_from_fn("--width", parse_width)?
-            .unwrap_or(10),
-        // Parses an optional value from `&OsStr` using a specified function.
-        output: pargs.opt_value_from_os_str("--input", parse_path)?,
-        // Parses a required free-standing/positional argument.
-        input: pargs.free_from_str()?,
-    };
+    let file: String = pargs.value_from_str("--file")?;
+    let login: String = pargs.value_from_str("--login")?;
+    let special: String = pargs
+        .value_from_str("--special")
+        .unwrap_or("_&#".to_string());
+    let length: u32 = pargs
+        .opt_value_from_fn("--length", parse_length)?
+        .unwrap_or(30);
+    let func: String = pargs
+        .value_from_str("--scramble-func")
+        .unwrap_or("md5".to_string());
 
-    // It's up to the caller what to do with the remaining arguments.
-    let remaining = pargs.finish();
-    if !remaining.is_empty() {
-        eprintln!("Warning: unused arguments left: {:?}.", remaining);
-    }
-
-    Ok(args)
+    Ok(AppArgs {
+        file,
+        login,
+        special,
+        length,
+        clip: pargs.contains(["-c", "--clip"]),
+        func,
+    })
 }
 
-fn parse_width(s: &str) -> Result<u32, &'static str> {
+fn parse_length(s: &str) -> Result<u32, &'static str> {
     s.parse().map_err(|_| "not a number")
-}
-
-fn parse_path(s: &std::ffi::OsStr) -> Result<std::path::PathBuf, &'static str> {
-    Ok(s.into())
 }
