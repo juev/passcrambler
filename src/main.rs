01afb439f9ede1e35e9ebf47903bbfad6f127908
Evsyukov Denis <denis@evsyukov.org>
2021-05-16 23:31:58 +0300

    [~] hasha to sha256, added aes encryption
    * change hash to sha3_256
    * added aes encryption
    
    Currently we have error:
    
    ```
    thread 'main' panicked at 'attempt to subtract with overflow',
    ```

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 use crate::cmd::parse_args;
+
+use rpassword::prompt_password_stdout;
 use std::path::Path;
-use rpassword::{prompt_password_stdout};
-use blake2::{Blake2b, Digest};
+
+use crypto::aes::{cbc_encryptor, KeySize};
+use crypto::blockmodes::PkcsPadding;
+use crypto::buffer::{ReadBuffer, WriteBuffer, BufferResult};
+use crypto::buffer::{RefReadBuffer, RefWriteBuffer};
+
+use crypto::digest::Digest;
+use crypto::sha3::Sha3;
 
 pub mod cmd;
 
@@ -21,13 +29,50 @@ fn main() {
 
     let password = prompt_password_stdout("Type password: ").unwrap();
 
+    let mut hasher = Sha3::sha3_256();
     // get password digest
-    let pass_digest = Blake2b::digest(password.as_ref());
+    hasher.input(password.as_ref());
+
+    let pass_digest = hasher.result_str();
 
     // get login digest
-    let login_digest = Blake2b::digest((&args.login).as_ref());
+    hasher.reset();
+    hasher.input((&args.login).as_ref());
+
+    let login_digest = hasher.result_str();
 
     // print login and password digest
-    println!("login digest: {:x}", login_digest);
-    println!("password digest: {:x}", pass_digest);
+    println!("login digest: {}", login_digest);
+    println!("password digest: {}", pass_digest);
+
+    let result = aes_encrypt(b"Hello world", pass_digest.as_ref(), login_digest.as_ref());
+
+    println!("{:?}", result);
+}
+
+pub fn aes_encrypt(data: &[u8], key: &[u8], iv: &[u8]) -> Vec<u8> {
+    let mut encryptor = cbc_encryptor(
+        KeySize::KeySize256,
+        key,
+        iv,
+        PkcsPadding,
+    );
+
+    let mut final_result = Vec::<u8>::new();
+    let mut buffer = [0; 4096];
+    let mut read_buffer = RefReadBuffer::new(data);
+    let mut write_buffer = RefWriteBuffer::new(&mut buffer);
+
+    loop {
+        // thread 'main' panicked at 'attempt to subtract with overflow'
+        let result = encryptor.encrypt(&mut read_buffer, &mut write_buffer, true);
+        final_result.extend(write_buffer.take_read_buffer().take_remaining().iter().map(|&i| i));
+        match result {
+
+            Ok(BufferResult::BufferUnderflow) => break,
+            Ok(BufferResult::BufferOverflow) => { }
+            Err(_) => { }
+        }    }
+
+    final_result
 }
