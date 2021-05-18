0c62d76b0ad62a169f4c257f5f620fdd2d697be2
Evsyukov Denis <denis@evsyukov.org>
2021-05-18 13:04:14 +0300

    [~] aes coding
    * using sha512
    * added addition aes coding
    * added base64 to result

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use crate::cmd::parse_args;
 
+extern crate base64;
+
 use rpassword::prompt_password_stdout;
 use std::path::Path;
 use std::fs;
+use std::str;
 
 use crypto::aes::{cbc_encryptor, KeySize};
 use crypto::blockmodes::{PkcsPadding};
@@ -11,6 +14,8 @@ use crypto::buffer::{RefReadBuffer, RefWriteBuffer};
 
 use crypto::digest::Digest;
 use crypto::sha3::Sha3;
+
+use base64::{encode};
 
 pub mod cmd;
 
@@ -33,28 +38,62 @@ fn main() {
 
     let password = prompt_password_stdout("Type password: ").unwrap();
 
-    let mut hasher = Sha3::sha3_256();
+    let mut hasher = Sha3::sha3_512();
     // get password digest
-    hasher.input(password.as_ref());
+    hasher.input(password.as_bytes());
     let pass_digest = hasher.result_str();
+    hasher.reset();
 
     // get login digest
-    hasher.reset();
-    hasher.input((&args.login).as_ref());
+    hasher.input((&args.login).as_bytes());
     let login_digest = hasher.result_str();
+    hasher.reset();
 
     // print login and password digest
     println!("login digest: {}", login_digest);
     println!("password digest: {}", pass_digest);
 
     // find aes coding our data
-    let result = aes_encrypt(
+    let aes_out1 = aes_encrypt(
         &*data.into_bytes(),
         pass_digest.as_bytes(),
         login_digest.as_bytes(),
     );
 
-    println!("{:?}", result);
+    hasher.input(&aes_out1);
+    let sha_digest = hasher.result_str();
+    hasher.reset();
+    println!("aes1 digest: {}", sha_digest);
+
+    let passlen = password.len() % sha_digest.len();
+    let key2 = &sha_digest[passlen..passlen+32];
+    println!("passlen: {}", passlen);
+    println!("key2: {}", key2);
+
+    // aes_out2 = aes_encrypt( key, key2, aes_out1 )
+    // find aes coding our data
+    let aes_out2 = aes_encrypt(
+        &*aes_out1,
+        pass_digest.as_bytes(),
+        key2.as_bytes(),
+    );
+
+    // start    = key[0] % len(aes_out2)
+    let start = password.chars().nth(0).unwrap() as usize % aes_out2.len();
+    // portion  = aes_out2[start:]
+    let portion = &aes_out2[start..aes_out2.len()];
+    // result   = hashlib.sha512(portion).digest()
+    hasher.input(&portion);
+    let result = hasher.result_str();
+    hasher.reset();
+    // longpass = base64.b64encode(result)
+    // longpass = longpass[0:args.length]
+    let base = encode(&result);
+    let longpass = str::from_utf8(&base.as_bytes()[..args.length as usize]).unwrap();
+    println!("longpass: {:}", longpass);
+    println!("longpass length: {}", longpass.len());
+    // longpass = convert_to_charset(longpass,  sorted(args.special, reverse=True))
+
 }
 
 pub fn aes_encrypt(data: &[u8], key: &[u8], iv: &[u8]) -> Vec<u8> {
